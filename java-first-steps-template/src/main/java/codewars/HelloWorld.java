6a86bf71ef5b4285c6619189a41adabf01e5167e
sergei <sergeikomsa94@gmail.com>
2022-10-31 17:09:02 +0300

    New file from new laptop

---
+++ java-first-steps-template/src/main/java/codewars/HelloWorld.java
@@ -0,0 +1,6 @@
+package codewars;
+public class HelloWorld{
+        public static void main(String[] args){
+            System.out.println("Добавляю коммит со стороннего устройства");
+        }
+}
