6e5f61a3ccfb2c8c8d8e3d2b4e57e4bb8816dbc8
Sergei <sergeikomsa94@gmail.com>
2022-10-31 16:21:04 +0300

    Add some solved tasks from codewars

--- java-first-steps-template/src/main/java/codewars/ArithmeticProgMissing.java
+++ java-first-steps-template/src/main/java/codewars/ArithmeticProgMissing.java
@@ -1,5 +1,18 @@
 package codewars;
 
 public class ArithmeticProgMissing {
+	
+	public static int findMissing(int[] numbers)
+	  {
+	     int step = (numbers[numbers.length - 1] - numbers[0])/numbers.length;
+	     int result = 0;
+	     if(step == 0){
+	         return numbers[0];
+	     }
+	     for(int i = 0; i < numbers.length - 1; i++)
+	          if(numbers[i + 1] - numbers[i] != step)
+	              result = numbers[i] + step;
 
+	    return result;      
+	  }  
 }
--- java-first-steps-template/src/main/java/codewars/StepInPrimes.java
+++ java-first-steps-template/src/main/java/codewars/StepInPrimes.java
@@ -1,5 +1,33 @@
 package codewars;
 
 public class StepInPrimes {
+	public static long[] step(int g, long m, long n){
+        long[] result = new long[2];
+        for(long i = m; i <= n - (long) g; i++){
+            if(checkPrimaryNum(i)){
+                if(checkPrimaryNum(i+(long)g)){
+                    result[0] = i;
+                    result[1] = i+(long)g;
+                    return result;
+                }
+            }
+        }
+        return null;
+        
+    }
+    
+    public static boolean checkPrimaryNum(long num){
+        long temp;
+        boolean isPrime = true;
+        
+        for(int i = 2; i <= num / 2; i++){
+            temp = num % i;
+            if(temp == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
 
 }
